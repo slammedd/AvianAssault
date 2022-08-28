c8491c157333a4f84ad94fc76bd11d6d03d0e393
Kyle James <kyleajames102@gmail.com>
2022-08-28 15:21:56 +0100

    Basic player controller implementation.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Player Stats")]
+    public float maxMoveSpeed = 10;
+    public float smoothTime = 0.3f;
+    public float minDistance = 2;
+    Vector2 currentVelocity;
+    public float turnSpeed = 45; // degrees per second
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        // Offsets the target position so that the object keeps its distance.
+        mousePosition += ((Vector2)transform.position - mousePosition).normalized * minDistance;
+        transform.position = Vector2.SmoothDamp(transform.position, mousePosition, ref currentVelocity, smoothTime, maxMoveSpeed);
+       
+        Vector3 mousePosition3d = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 direction = mousePosition3d - transform.position;
+        float angle = Vector2.SignedAngle(Vector2.up, direction);
+        Vector3 targetRotation = new Vector3(0, 0, angle);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(targetRotation), turnSpeed * Time.deltaTime);
+    }
+}
